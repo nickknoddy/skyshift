28bde070feda8de7d462dfaa617dbc5cd079b868
Nikhil Naidu <nikhilnaidun9n@gmail.com>
2025-04-14 07:03:25 +0000

    Use custom config with fiber app

--- main.go
+++ main.go
@@ -4,17 +4,37 @@ import (
 	"log/slog"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/nickknoddy/skyshift/pkg/configs"
+	"github.com/spf13/viper"
 )
+
+func init() {
+	// Read env file
+	readEnvVar(".env")
+}
+
+func readEnvVar(fileName string) {
+	viper.SetConfigFile(fileName)
+
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		slog.Error("Error while reading env variables", "Reason:", err)
+	}
+}
 
 func main() {
 
-	// Define a new Fiber app
-	app := fiber.New()
+	// Define Fiber config.
+	config := configs.FiberConfig()
+
+	// Define a new Fiber app with config.
+	app := fiber.New(config)
 
 	app.Get("/ping", PingCheck)
 
 	if err := app.Listen(":8000"); err != nil {
-		slog.Error("Server is not starting.", "Reason:", err.Error())
+		slog.Error("Server is not starting.", "Reason:", err)
 	}
 }
 
