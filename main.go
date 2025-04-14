88704658d9003d53598bfc8867cae24ea9c396ea
Nikhil Naidu <nikhilnaidun9n@gmail.com>
2025-04-14 06:58:31 +0000

    Add simple fiber app with ping check endpoint

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("hello world")
+
+	// Define a new Fiber app
+	app := fiber.New()
+
+	app.Get("/ping", PingCheck)
+
+	if err := app.Listen(":8000"); err != nil {
+		slog.Error("Server is not starting.", "Reason:", err.Error())
+	}
+}
+
+func PingCheck(c *fiber.Ctx) error {
+	return c.SendString("Hello World")
 }
