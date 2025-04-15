dc96e79b9fb0c60b027e50d77a5568aad96fb883
Nikhil Naidu <nikhilnaidun9n@gmail.com>
2025-04-15 15:23:02 +0000

    Add sharpen transformation

--- app/controllers/transform_controller.go
+++ app/controllers/transform_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"bytes"
 	"fmt"
+	"image/jpeg"
 	"image/png"
 	"log/slog"
 	"strconv"
@@ -14,31 +15,52 @@ import (
 func Transform(c *fiber.Ctx) error {
 
 	fileName := c.Params("fileName")
-
-	w, err := strconv.Atoi(c.Query("w"))
-	if err != nil {
-		slog.Error("width should be an int")
-		return c.Status(400).SendString("width should be an int")
-	}
-	h, err := strconv.Atoi(c.Query("h"))
-	if err != nil {
-		slog.Error("height should be an int")
-		return c.Status(400).SendString("height should be an int")
-	}
+	var buf bytes.Buffer
 
 	image, err := imaging.Open(fmt.Sprintf("tmp/%s", fileName))
 	if err != nil {
-		slog.Error("failed to open image")
+		slog.Error("failed to open image", "reason", err)
 		return c.Status(500).SendString("Failed to process image")
 	}
 
-	transformedImage := imaging.Resize(image, w, h, imaging.Lanczos)
+	if c.Query("w") != "" && c.Query("w") != "" {
+		w, err := strconv.Atoi(c.Query("w"))
+		if err != nil {
+			slog.Error("width should be an int")
+			return c.Status(400).SendString("width should be an int")
+		}
+		h, err := strconv.Atoi(c.Query("h"))
+		if err != nil {
+			slog.Error("height should be an int")
+			return c.Status(400).SendString("height should be an int")
+		}
 
-	var buf bytes.Buffer
-	if err := png.Encode(&buf, transformedImage); err != nil {
-		return c.Status(500).SendString("Failed to process image")
+		transformedImage := imaging.Resize(image, w, h, imaging.Lanczos)
+		if err := png.Encode(&buf, transformedImage); err != nil {
+			return c.Status(500).SendString("Failed to process image")
+		}
+
+		c.Type("png")
+		return c.Send(buf.Bytes())
 	}
 
-	c.Type("png")
+	sharpen, err := strconv.ParseFloat(c.Query("sharpen"), 64)
+	if err != nil {
+		slog.Error("sharpen should be an int")
+		return c.Status(400).SendString("sharpen should be an int")
+	}
+
+	if sharpen > 0 {
+		img := imaging.Resize(image, image.Bounds().Dx()/2, image.Bounds().Dy()/2, imaging.Lanczos)
+		slog.Info("sharpening start", "sharpen value: ", sharpen)
+		sharpenedImage := imaging.Sharpen(img, sharpen)
+		slog.Info("sharpening complete")
+		if err := jpeg.Encode(&buf, sharpenedImage, &jpeg.Options{Quality: 50}); err != nil {
+			return c.Status(500).SendString("Failed to process image")
+		}
+
+		c.Type("jpg")
+	}
+
 	return c.Send(buf.Bytes())
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/nickknoddy/skyshift
 go 1.24.2
 
 require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/gofiber/fiber/v2 v2.52.6
+	github.com/spf13/viper v1.20.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
-	github.com/disintegration/imaging v1.6.2 // indirect
 	github.com/fsnotify/fsnotify v1.8.0 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
-	github.com/gofiber/fiber/v2 v2.52.6 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
@@ -20,7 +24,6 @@ require (
 	github.com/spf13/afero v1.12.0 // indirect
 	github.com/spf13/cast v1.7.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
-	github.com/spf13/viper v1.20.1 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
