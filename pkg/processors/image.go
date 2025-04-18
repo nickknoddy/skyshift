0a88dcf2a47f6d1d1a3629000525d4695669a262
Nikhil Naidu <nikhilnaidun9n@gmail.com>
2025-04-18 15:01:55 +0000

    Add convert image type helper func

--- pkg/processors/image.go
+++ pkg/processors/image.go
@@ -1,6 +1,10 @@
 package processors
 
 import (
+	"bytes"
+	"image"
+	"image/jpeg"
+	"image/png"
 	"strings"
 )
 
@@ -8,4 +12,21 @@ func InferImageType(filename string) string {
 
 	imageType := strings.Split(filename, ".")
 	return imageType[len(imageType)-1]
+}
+
+func convertImageFormat(image *image.NRGBA, format string, quality int) ([]byte, error) {
+
+	var buf bytes.Buffer
+
+	if format == "png" {
+		if err := png.Encode(&buf, image); err != nil {
+			return nil, err
+		}
+	} else {
+		if err := jpeg.Encode(&buf, image, &jpeg.Options{Quality: quality}); err != nil {
+			return nil, err
+		}
+	}
+
+	return buf.Bytes(), nil
 }
